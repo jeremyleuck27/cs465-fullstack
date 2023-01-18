97824240c1e99142540eb7da5415e4f1634c65e8
jeremyleuck27 <jeremy.leuck@gmail.com>
2023-01-18 14:11:25 -0600

    Convert travel template froom static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+//not best practice on practical site
+//because you do not want to read file every time use click on site
+//BEST PRACTICE on startup, read file and store in cache that can be called
+//at anytime
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', {title, pageTitle, trips});
 };
 
 module.exports = {
