de208987b1044293681a9ac4c764b239eb38005f
jeremyleuck27 <jeremy.leuck@gmail.com>
2023-02-01 10:26:34 -0600

    Create new application API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,15 +1,47 @@
-const fs = require('fs');
-//not best practice on practical site
-//because you do not want to read file every time use click on site
-//BEST PRACTICE on startup, read file and store in cache that can be called
-//at anytime
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+// internal method to render the travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel', {
+            title: pageTitle,
+            trips: responseBody,
+            message
+    });
+}
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    console.info('>> travelController.travlList calling' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, {statusCode }, body) => {
+            if(err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
 };
 
 module.exports = {
-    travel
+    travelList
 };
