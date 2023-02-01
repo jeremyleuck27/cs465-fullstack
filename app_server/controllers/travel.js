669c4584d948364741a05f53ab4080c9845daa64
jeremyleuck27 <jeremy.leuck@gmail.com>
2023-01-31 19:59:12 -0600

    Module 4

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -7,7 +7,7 @@ const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title, pageTitle, trips});
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
